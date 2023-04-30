4374aa8596daaa2b317a3b155f431dea7e0de1eb
folkengine <gaoler@electronicpanopticon.com>
2023-04-30 15:10:45 -0700

    fixed main

--- cmd/rngo/main.go
+++ cmd/rngo/main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/folkengine/rngo/pkg/rngo"
 )
 
 func main() {
-	fmt.Println(rngo.Boop())
+
+	elvenFlag := flag.Bool("elven", false, "Create Elven names")
+	fantasyFlag := flag.Bool("fantasy", false, "Create Fantasy names")
+	//goblinFlag := flag.Bool("goblin", false, "Create Goblin names")
+	//romanFlag := flag.Bool("roman", true, "Create Roman names")
+	flag.Parse()
+
+	var myname rngo.Rngo
+
+	if *elvenFlag {
+		myname = rngo.New(rngo.ElvenMap)
+	} else if *fantasyFlag {
+		myname = rngo.New(rngo.FantasyMap)
+	} else {
+		myname = rngo.New(rngo.GoblinMap)
+	}
+
+	fmt.Println(myname.FirstLast())
 }
